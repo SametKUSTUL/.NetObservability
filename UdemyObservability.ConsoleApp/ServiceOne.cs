4d8a7bb4ccb0532b3fe4eeb4fe1de5f662cc5060
skustul <samet.kustul@nesine.com>
2024-07-04 15:27:28 +0300

    activity tags and activity status

--- UdemyObservability.ConsoleApp/ServiceOne.cs
+++ UdemyObservability.ConsoleApp/ServiceOne.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,29 @@ namespace UdemyObservability.ConsoleApp
         internal async Task<int> MakeRequestToGoogle()
         {
             using var activity = ActivitySourceProvider.Source.StartActivity(kind:System.Diagnostics.ActivityKind.Producer,name:"CustomMakeRequestToGoogle");
-            var result = await httpClient.GetAsync("https://www.google.com");
-            var responseContent = await result.Content.ReadAsStringAsync();
-            return responseContent.Length;
+
+            try
+            {
+                var eventTags = new ActivityTagsCollection();
+                eventTags.Add("userId", 30);
+
+                activity?.AddEvent(new("GoogleIstekBasladi", tags: eventTags));
+
+                var result = await httpClient.GetAsync("https://www.google.com");
+                var responseContent = await result.Content.ReadAsStringAsync();
+                eventTags.Add("google body lenght", responseContent.Length);
+                activity?.AddEvent(new ActivityEvent("Google istek tamamlandi", tags: eventTags));
+
+                return responseContent.Length;
+            }
+            catch (Exception ex)
+            {
+                activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+                return -1;
+            }
+
+
+           
         }
     }
 }
